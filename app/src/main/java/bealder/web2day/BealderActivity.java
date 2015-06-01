fb296304faae67fe281b8158ac683760bd526b2e
Neamar <neamar@neamar.fr>
2015-06-01 18:04:25 +0200

    Contact us button

--- app/src/main/java/bealder/web2day/BealderActivity.java
+++ app/src/main/java/bealder/web2day/BealderActivity.java
@@ -1,16 +1,25 @@
 package bealder.web2day;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 
 public class BealderActivity extends Activity {
-		@Override
-		protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-            setContentView(R.layout.activity_bealder);
+        setContentView(R.layout.activity_bealder);
 
-            new MixinMenuActivity().addMenuListeners(this);
-        }
+        new MixinMenuActivity().addMenuListeners(this);
+    }
+
+    public void contactUs(View contactUs) {
+        Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                "mailto", "contact@bealder.com", null));
+        startActivity(Intent.createChooser(emailIntent, getString(R.string.send_email)));
+    }
 }
